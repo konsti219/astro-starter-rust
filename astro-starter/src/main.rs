3fef12b73e968c3d9b9bf4cc3669026853c0408f
konsti219 <37149441+konsti219@users.noreply.github.com>
2021-02-23 17:13:22 +0000

    add configuration options

--- astro-starter/src/main.rs
+++ astro-starter/src/main.rs
@@ -1,3 +1,30 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+#[derive(Debug)]
+enum ServerType {
+    Local,
+    Remote,
+}
+#[derive(Debug)]
+struct ServerConfig {
+    id: String,
+    server_type: ServerType,
+    name: String,
+    server_addr: SocketAddr,
+    console_addr: SocketAddr,
+    console_password: String,
+    whitelist: bool,
+    save_interval: usize,
+    backup_saves: bool,
+    backup_interval: usize,
+    enable_astrochat_integration: bool,
+}
+#[derive(Debug)]
+struct StarterConfig {
+    webserver_port: u16,
+    servers: Vec<ServerConfig>,
+}
+
 fn main() {
     println!("Do stuff");
 }
